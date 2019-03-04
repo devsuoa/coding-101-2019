061c1567b9d3d6646e2ca7feaf7e631e06cf2ed7
Jordan Sim-Smith <jordansimsmith@gmail.com>
2019-03-04 19:20:15 +1300

    Completed endpoint for sentiment analyis

--- api/sentiment-post/app.js
+++ api/sentiment-post/app.js
@@ -1,12 +1,39 @@
-exports.handler = async (event, context, callback) => {
+const AWS = require('aws-sdk')
 
-  response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'hello from sentiment-post'
-    })
-  }
+exports.handler = (event, context, callback) => {
 
-  // success
-  callback(null, response)
+  // create client
+  const comprehend = new AWS.Comprehend();
+
+  // get texts to analyse from request
+  let request = JSON.parse(event.body);
+
+  // define params to detect sentiment
+  let params = {
+    LanguageCode: 'en',
+    TextList: request.texts
+  };
+
+  // get sentiment
+  comprehend.batchDetectSentiment(params, (err, data) => {
+
+    // check if the service returend an error
+    if (err) {
+      let response = {
+        statusCode: 500,
+        body: JSON.stringify(err)
+      };
+
+      callback(null, response);
+    }
+
+    // no errors from service
+    let sentiments = data.ResultList.map(a => a.Sentiment);
+    let response = {
+      statusCode: 200,
+      body: JSON.stringify(sentiments)
+    };
+
+    callback(null, response);
+  })
 }
