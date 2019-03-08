af1125bbb77967a2a1514e9d41a3b31e76ba3fe2
Jordan Sim-Smith <jordansimsmith@gmail.com>
2019-03-08 15:23:45 +1300

    Wrote get tweets endpoint

--- api/tweets-get/app.js
+++ api/tweets-get/app.js
@@ -1,12 +1,77 @@
+const axios = require('axios');
+
+const apiKey = process.env.TWITTER_API_KEY;
+const secretKey = process.env.TWITTER_SECRET_KEY;
+
 exports.handler = async (event, context, callback) => {
 
-  response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'hello from tweets-get'
-    })
+  // get the display name from request
+  if (event.queryStringParameters === null || event.queryStringParameters.display_name === undefined) {
+    const response = {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "display_name required parameter omitted"
+      })
+    };
+    callback(null, response);
+  }
+  const displayName = event.queryStringParameters.display_name;
+
+  // get bearer token
+  let token;
+  try {
+    const resp = await axios.post(
+      'https://api.twitter.com/oauth2/token?grant_type=client_credentials', {}, {
+        auth: {
+          username: apiKey,
+          password: secretKey
+        }
+      });
+
+    token = resp.data.access_token;
+  } catch (e) {
+    console.error(e);
+
+    const response = {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Failed to authenticate"
+      })
+    };
+    callback(null, response);
   }
 
-  // success
-  callback(null, response)
-}
+  // get tweets
+  try {
+    let resp = await axios.get(
+      `https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=${displayName}`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+
+    // extract tweet bodies
+    let tweets = resp.data.map(a => a.text);
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        tweets: tweets
+      })
+    };
+
+    // success
+    callback(null, response);
+
+  } catch (e) {
+    console.error(e);
+
+    const response = {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Failed to get tweets"
+      })
+    };
+    callback(null, response);
+  }
+};
